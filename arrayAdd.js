438c38dde8e110b35c12bff86fc1a6adc136d737
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-27 08:25:55 +0600

    commit

--- arrayAdd.js
+++ arrayAdd.js
@@ -2,7 +2,7 @@ function arrayTotal (numbers){
     let sum = 0; 
     for(let i = 0; i < numbers.length; i++)
     {const element = numbers[i];
-         sum += element;
+         
         }
          return sum;
         }; 
