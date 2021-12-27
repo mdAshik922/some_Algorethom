12968f3b2bca7c61ff9d4ab8066bc9f052398796
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-27 10:53:19 +0600

    commit

--- Seliction_sorting.js
+++ Seliction_sorting.js
@@ -4,8 +4,8 @@ function Selection_sorting(array){
     for(let i = 0; i < array.length; i++ ){
        let min = i;
        for ( let j = i; j < array.length; j++){
-           if(array[j] < array[min]){
-               
+           if(array[j] ){
+               min = j;
            };
        }
        [array[i], array[min]] = [array[min], array[i]];
