aad56a3af2781a6eb4bc3d6cdac41a37b904f998
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-27 10:53:44 +0600

    commit

--- Seliction_sorting.js
+++ Seliction_sorting.js
@@ -1,7 +1,7 @@
 const number = [11, 32, 100, 125, 54, 61, 65, 78, 450, 600];
 
 function Selection_sorting(array){
-    for(let i = 0; i < array.length; i++ ){
+    for(let i = ; i < array.length; i++ ){
        let min = i;
        for ( let j = i; j < array.length; j++){
            if(array[j] < array[min]){
