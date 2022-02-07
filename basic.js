12a844d8e95bf3a3fb3f631cacf82a1398a39a31
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-07 09:17:39 +0600

    commit

--- basic.js
+++ basic.js
@@ -1,4 +1,7 @@
 var a = 10.38908;
 console.log(Math.floor(a));
 console.log(Math.ceil(a));
-console.log(Math.round(a));
+console.log(Math.round(Math.random()*10));
+console.log(Math.random()*10);
+const date = new Date();
+console.log(date.getFullYear());
